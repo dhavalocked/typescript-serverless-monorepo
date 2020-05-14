c3d780f857580eba7b810e36ab48d513bfc52d69
dhavalocked <pdhaval1994.dp@gmail.com>
2020-05-14 16:14:39 +0530

    added test cases

---
+++ services/user/tests/auth.test.ts
@@ -0,0 +1,15 @@
+import * as handler from '../src/user';
+
+test('hello', async () => {
+
+    const event = { data: 'hello' };
+    const context = {};
+
+    const callback = (error, response) => {
+        expect(response.statusCode).toEqual(200);
+        expect(error).toBe(null);
+    };
+
+    await handler.hello(event, context, callback);
+    return;
+});
